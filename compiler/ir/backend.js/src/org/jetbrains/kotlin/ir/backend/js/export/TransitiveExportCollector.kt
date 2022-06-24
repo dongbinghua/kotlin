f6023f1e6f2d3b8a9b1aff619adabbb8c2093e57
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-06-24 15:56:13 +0200

    fix: save initial export hierarchy inside strict implicit export model.

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/export/TransitiveExportCollector.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/export/TransitiveExportCollector.kt
@@ -13,6 +13,10 @@ import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.symbols.IrTypeParameterSymbol
 import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
+import org.jetbrains.kotlin.ir.backend.js.utils.isJsImplicitExport
+import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.types.superTypes
+import org.jetbrains.kotlin.ir.util.isInterface
 
 private typealias SubstitutionMap = Map<IrTypeParameterSymbol, IrTypeArgument>
 
@@ -41,7 +45,13 @@ class TransitiveExportCollector(val context: JsIrBackendContext) {
         val owner = classifier.owner as? IrClass ?: return emptySet()
         return when {
             isBuiltInClass(owner) || isStdLibClass(owner) -> emptySet()
-            owner.isExported(context) -> setOf(getSubstitutionFrom(typeSubstitutionMap) as IrType)
+            owner.isExportedImplicitlyOrExplicitly(context) -> setOf(getSubstitutionFrom(typeSubstitutionMap) as IrType).run {
+                if (!owner.isInterface && owner.isJsImplicitExport()) {
+                    plus(collectSuperTransitiveHierarchy(typeSubstitutionMap))
+                } else {
+                    this
+                }
+            }
             else -> collectSuperTransitiveHierarchy(typeSubstitutionMap)
         }
     }
